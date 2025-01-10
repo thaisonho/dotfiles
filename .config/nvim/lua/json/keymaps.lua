a5826c7356e20f6489f1c949b32cff91ce2e445c
thaisonho <hothaison2005@gmail.com>
2025-01-10 23:56:44 +0700

    Add .tmux.conf

--- .config/nvim/lua/json/keymaps.lua
+++ .config/nvim/lua/json/keymaps.lua
@@ -2,10 +2,10 @@ local M = {}
 
 function M.basic_keymaps()
     vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { noremap = true, silent = true })
-    vim.keymap.set('n', '<leader>h', '<C-w>h', { noremap = true, silent = true})
-    vim.keymap.set('n', '<leader>l', '<C-w>l', { noremap = true, silent = true})
-    vim.keymap.set('n', '<leader>j', '<C-w>j', { noremap = true, silent = true})
-    vim.keymap.set('n', '<leader>k', '<C-w>k', { noremap = true, silent = true})
+    vim.keymap.set('n', '<leader>h', '<C-w>h', { desc = 'Navigate to left window', noremap = true, silent = true})
+    vim.keymap.set('n', '<leader>l', '<C-w>l', { desc = 'Navigate to right window',noremap = true, silent = true})
+    vim.keymap.set('n', '<leader>j', '<C-w>j', { desc = 'Navigate to top window', noremap = true, silent = true})
+    vim.keymap.set('n', '<leader>k', '<C-w>k', { desc = 'Navigate to bottom window', noremap = true, silent = true})
 end
 
 return M
